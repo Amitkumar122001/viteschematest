906bde7b2a1f470f7be9de5dfb1be63296561433
Amit Kumar <amit.kumar@aaveg.com>
2025-05-06 15:42:41 +0530

    schema fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const schemaMarkup = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Your Company Name",
+    url: "https://www.example.com",
+    logo: "https://www.example.com/logo.png",
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+1-123-456-7890",
+      contactType: "customer service",
+    },
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <script type="application/lg+json">
-        {`{
+      <Helmet>
+        <script type="application/lg+json">
+          {`{
               "@context": "https://schema.org",
               "@type": "WebPage",
               "name": "About Us",
@@ -70,7 +63,29 @@ function App() {
 }
               
               `}
-      </script>
+        </script>
+      </Helmet>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {/*  */}
     </>
   );
 }
